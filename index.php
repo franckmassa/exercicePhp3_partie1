a0a3d249321c4a2cddea87a07d24861683bc0dc2
massa <massafranck@gmail.com>
2018-07-25 20:03:04 +0200

    initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$km = 1;
+$km = 3;
+$km = 5;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice3</title>
+  </head>
+  <body>
+    <p>
+      <?php
+    echo $km;
+       ?>
+    </body>
+    </p>
+</html>
