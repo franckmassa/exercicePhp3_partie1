9424c7588c1d6e7c4d68f4ddcbd3df3db73febaa
massa <massafranck@gmail.com>
2018-07-26 21:17:11 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,19 +1,29 @@
-<?php
-$km = 1;
-$km = 3;
-$km = 5;
-?>
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice3</title>
-  </head>
-  <body>
-    <p>
-      <?php
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice3</title>
+</head>
+<body>
+  <p>
+    <?php
+    $km = 1;
     echo $km;
-       ?>
-    </body>
-    </p>
+    ?>
+  </body>
+</p>
+<p>
+  <?php
+  $km = 3;
+  echo $km;
+  ?>
+</body>
+</p>
+<p>
+  <?php
+  $km = 125;
+  echo $km;
+  ?>
+</body>
+</p>
 </html>
